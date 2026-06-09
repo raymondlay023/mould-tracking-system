61228c4426e3aac9ef3ee6147da0db7823ca096d
Raymond Lay <raymondlay023@gmail.com>
2026-06-09 10:09:41 +0700

    chore: update Guzzle components and Laravel framework to latest versions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             // MasterDataSeeder::class,
-            DemoDataSeeder::class,
+            // DemoDataSeeder::class,
         ]);
     }
 }
