a21a59a5d5c7b16fa00d4f8dd25ea3b11c8fde48
Raymond Lay <raymondlay023@gmail.com>
2026-06-22 08:34:19 +0700

    fix: remove opacity hover effect on action buttons in mould index table

--- resources/views/livewire/moulds/index.blade.php
+++ resources/views/livewire/moulds/index.blade.php
@@ -194,7 +194,7 @@
                                         @endif
                                     </td>
                                     <td class="text-right py-3 px-4">
-                                        <div class="flex items-center justify-end gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+                                        <div class="flex items-center justify-end gap-2">
                                             <button wire:click="edit('{{ $m->id }}')" class="p-1 rounded hover:bg-blue-100 text-blue-600 transition-colors" title="Edit">
                                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
                                             </button>
